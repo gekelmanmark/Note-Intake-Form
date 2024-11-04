d8e92eb39881ebb56c4ad66ab5276a20539f2f98
Mark Gekelman <gekelmanmark@gmail.com>
2024-11-04 05:00:04 -0800

    Dark mode functionality added to navbar in js

--- script.js
+++ script.js
@@ -217,6 +217,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 ...document.querySelectorAll('select'),
                 ...document.querySelectorAll('input'),
                 ...document.querySelectorAll('option'),
+                ...document.querySelectorAll('nav'),
                 ...document.querySelectorAll('button')
             ];
 
